2d1aa169fb70e72a125c7f687529b7384a4393ba
Toby Thurston <tobythurston@mac.mynetworksettings.com>
2025-06-11 22:12:50 -0400

    fix: add default export to features page

---
+++ next.config.ts
@@ -0,0 +1,14 @@
+import type { NextConfig } from 'next';
+
+/** @type {import('next').NextConfig} */
+const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
+};
+
+export default nextConfig;
+
+
+
