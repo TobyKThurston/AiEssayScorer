9ec0adcaefcb229962021da4fa3135d34a17e3ca
Toby Thurston <tobythurston@mac.mynetworksettings.com>
2025-06-11 21:46:06 -0400

    fix: use JS next.config so ignoreDuringBuilds is respected

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    //  ✅  Turn ALL ESLint “errors” into warnings during `next build`
+    ignoreDuringBuilds: true,
+  },
+  // …add any other Next.js settings here…
+};
+
+module.exports = nextConfig;
+
+
+
+
