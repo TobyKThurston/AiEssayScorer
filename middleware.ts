40fe27fc5ddcfc4eecba46a1c8777d2b61523d9d
Toby Thurston <tobythurston@mac.mynetworksettings.com>
2025-06-11 22:46:42 -0400

    fix: ensure Clerk middleware matches /api/* routes

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,12 @@
+// middleware.ts
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
 export default clerkMiddleware();
 
 export const config = {
   matcher: [
-    "/((?!_next|.*\\..*).*)",
-    "/(api|trpc)(.*)",
+    "/api/(.*)",
+    "/((?!.+\\.[\\w]+$|_next).*)",
   ],
 };
 
